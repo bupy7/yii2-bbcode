9aee44ebc023ae5758292ce0cc378311195df789
Vasilij Belosludcev <bupy765@gmail.com>
2015-01-30 22:37:09 +0500

    added center, right, left tags

--- Parser.php
+++ Parser.php
@@ -5,41 +5,10 @@ namespace bupy7\bbcode;
 /**
  * @inheritdoc
  * 
- * Support BB-codes: 
- * [b]bold[/b]
- * [i]italic[/i]
- * [u]underlining[/u]
- * [list] with marker
- *     [*] first
- *     [*] second
- * [/list]
- * [list=1] with number 
- *     [*] first
- *     [*] second
- * [/list]
- * [url=http://domain.zone]This is link[/url]
- * [color=red]color![/color]
- * [img=This is image]http://link.to/image.png[/img]
- * [p]paragraph[/p]
- * [blockquote]blockquote[/blockquote]
- * [h=1|2|3|4|5|6]header[/h=1|2|3|4|5|6]
- * 
  * @author Belosludcev Vasilij http://github.com/bupy7
  */
 class Parser extends \JBBCode\Parser
-{
-    
-    /**
-     * @inheritdoc
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        
-        $this->addCodeDefinition(new definitions\ListMarkerCodeDefinition);
-        $this->addCodeDefinition(new definitions\ListNumberCodeDefinition);
-    }
-    
+{    
 
     /**
      * Insert <br /> tag to new line.
--- definitions/DefaultCodeDefinitionSet.php
+++ definitions/DefaultCodeDefinitionSet.php
@@ -7,6 +7,27 @@ use bupy7\bbcode\validators\NumberValidator;
 
 /**
  * Provides a default set of common bbcode definitions.
+ * Support BB-codes: 
+ * [b]bold[/b]
+ * [i]italic[/i]
+ * [u]underlining[/u]
+ * [list] with marker
+ *     [*] first
+ *     [*] second
+ * [/list]
+ * [list=1] with number 
+ *     [*] first
+ *     [*] second
+ * [/list]
+ * [url=http://domain.zone]This is link[/url]
+ * [color=red]color![/color]
+ * [img=This is image]http://link.to/image.png[/img]
+ * [p]paragraph[/p]
+ * [blockquote]blockquote[/blockquote]
+ * [h=1|2|3|4|5|6]header[/h=1|2|3|4|5|6]
+ * [center]text by center[/center]
+ * [right]text align by right[/right]
+ * [left]text align by left[/left]
  *
  * @author Belosludcev Vasilij http://github.com/bupy7
  */
@@ -43,6 +64,28 @@ class DefaultCodeDefinitionSet extends \JBBCode\DefaultCodeDefinitionSet
                 'max' => 6,
             ]));
         array_push($this->definitions, $builder->build());
+        
+        /* [center] align text by center */
+        $builder = new CodeDefinitionBuilder('center', '<span style="text-align:center">{param}</span>');
+        $builder->setParseContent(true);
+        array_push($this->definitions, $builder->build());
+        
+        /* [left] align text by left of edge */
+        $builder = new CodeDefinitionBuilder('left', '<span style="text-align:left">{param}</span>');
+        $builder->setParseContent(true);
+        array_push($this->definitions, $builder->build());
+        
+        /* [right] align text by right of edge */
+        $builder = new CodeDefinitionBuilder('right', '<span style="text-align:right">{param}</span>');
+        $builder->setParseContent(true);
+        array_push($this->definitions, $builder->build());
+        
+        
+        /* [list] with marker */
+        array_push($this->definitions, new ListMarkerCodeDefinition);
+        
+        /* [list=1] with number */
+        array_push($this->definitions, new ListNumberCodeDefinition);
     }
 
 }
